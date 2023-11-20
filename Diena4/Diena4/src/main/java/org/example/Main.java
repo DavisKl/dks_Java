d4f0c55dd6fd90122baf9515075dcfcff8a7ee34
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-20 21:03:38 +0200

    Update Main.java

--- Diena4/Diena4/src/main/java/org/example/Main.java
+++ Diena4/Diena4/src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        String a = "aaaa";
@@ -6,9 +8,47 @@ public class Main {
 //        task1(a, b, c);
 //        task2("aaa", "bbb", "ccc");
 
-        homework1(10);
+//        homework1(10);
+        int sum = task3(4);
+        System.out.println(sum);
+
+
+//        int result = sampleWithReturn();
+//        System.out.println(result);
+
+
 
     }
+
+    public static int task3(int count){
+        if(count < 1){
+            System.out.println("Kļūda");
+            return 0;
+        }
+
+        int sum = 0;
+
+        for(int i = 1; i <= count; i++){
+            sum = sum + i;
+        }
+
+        return sum;
+        }
+    }
+    public static int sampleWithReturn(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadi skaitli");
+        int a = sc.nextInt();
+
+        System.out.println("Ievadi skaitli");
+        int b = sc.nextInt();
+
+        int c = a + b;
+
+        return c;
+    }
+
+
     public static void task1(String a, String b, String c){
         System.out.println(c+b+a);
     }
