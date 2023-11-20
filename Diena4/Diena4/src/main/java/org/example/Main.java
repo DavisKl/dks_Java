d39478f154fc3426068a8985b2f86188ab2dc53f
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-20 19:47:15 +0200

    Update Main.java

--- Diena4/Diena4/src/main/java/org/example/Main.java
+++ Diena4/Diena4/src/main/java/org/example/Main.java
@@ -4,9 +4,19 @@ public class Main {
         String b = "bbbb";
         String c = "cccc";
         task1(a, b, c);
+        task2("aaa", "bbb", "ccc");
 
     }
     public static void task1(String a, String b, String c){
         System.out.println(c+b+a);
+    }
+    public static void task2 (String d, String e, String f){
+        int len = d.length() + e.length() + f.length();
+        if(len > 15){
+            System.out.println(d + f);
+        }else {
+            System.out.println(f + e + d);
+        }
+
     }
 }
