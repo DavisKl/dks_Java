f723a87a2625c2b1412fd82be8c63f8067ba79fd
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-20 19:59:41 +0200

    Update Main.java

--- Diena4/Diena4/src/main/java/org/example/Main.java
+++ Diena4/Diena4/src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        String a = "aaaa";
-        String b = "bbbb";
-        String c = "cccc";
-        task1(a, b, c);
-        task2("aaa", "bbb", "ccc");
+//        String a = "aaaa";
+//        String b = "bbbb";
+//        String c = "cccc";
+//        task1(a, b, c);
+//        task2("aaa", "bbb", "ccc");
+
+        homework1(10);
 
     }
     public static void task1(String a, String b, String c){
@@ -19,4 +21,19 @@ public class Main {
         }
 
     }
+    public static void homework1(int a) {
+        String symbol = "";
+        for (int i = 0; i < a; i++) {
+            symbol = symbol + "$";
+            System.out.println(symbol);
+        }
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
+
 }
