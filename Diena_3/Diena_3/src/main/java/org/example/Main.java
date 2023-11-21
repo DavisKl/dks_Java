19cd680e55b9254206599a670080323be552066c
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-21 16:25:09 +0200

    Update Main.java

--- Diena_3/Diena_3/src/main/java/org/example/Main.java
+++ Diena_3/Diena_3/src/main/java/org/example/Main.java
@@ -4,11 +4,89 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        sample3();
+        task4();
+//        sample6();
+//        sample5();
+//        task3();;
+//        sample4();
+//        sample3();
 //        task2();
 //        sample2();
 //        sample1();
 //        task1();
+    }
+
+    public static void task4(){
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        String random = "";
+
+        System.out.println("Ievadi vārdu!");
+        input = sc.nextLine();
+        System.out.println("Tu ievadīji vārdu - " + input);
+
+        while(!input.equals("")){
+            System.out.println("Ievadiet kādu vārdu!");
+            random = sc.nextLine();
+            System.out.println(random);
+            if(random.equals(input)){
+                break;
+            }
+
+        }
+    }
+
+
+
+
+    public static void sample6(){
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int input = 0;
+
+        while(input>=0){
+
+            System.out.println("Ievadiet skaitli!");
+            input = sc.nextInt();
+            if(input < 0){
+                break;
+            }
+
+            sum = sum + input;
+
+        }
+        System.out.println("Kopējā summa ir " + sum);
+    }
+
+    public static void sample5(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadīt simbolu virkni");
+        String input = "";
+        while (!input.equals("exit")){
+            System.out.println("Hello world!");
+            System.out.println("Ievadīt simbolu virkni!");
+            input = sc.nextLine();
+        }
+    }
+
+    public static void task3(){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Lūdzu ievadiet skaitli!");
+            int number = sc.nextInt();
+            if( number > 0){
+                sum = sum + number;
+                System.out.println(number);
+                System.out.println("-------------");
+            }else{
+                System.out.println("Kļūda!");
+            }
+        }
+        System.out.println("Summa ir " + sum);
+
     }
 
 
@@ -61,5 +139,39 @@ public class Main {
     public static void sample3(){
         Scanner sc = new Scanner(System.in);
 
+
+            for(int i = 0; i < 5; i++){
+                System.out.println("Lūdzu ievadiet skaitli");
+                int number = sc.nextInt();
+
+                if(number == 0) {
+                    System.out.println("KĻŪDA!");
+                }else if(number == 4){
+                    System.out.println("Hello world!");
+                }else{
+                    System.out.println("Hello world2!");
+                }
+            }
+        }
+
+
+
+
+
+    public static void sample4(){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Lūdzu ievadiet skaitli!");
+        int number = sc.nextInt();
+
+        if (number == 5){
+            for(int i = 0; i < 5; i++){
+                System.out.println("Hello!");
+            }
+        }else{
+            System.out.println("Kļūda");
+        }
+
     }
+
 }
