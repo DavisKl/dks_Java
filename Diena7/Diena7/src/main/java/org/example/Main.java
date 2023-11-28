7a6a34a95970905472098973ee6dba0583da234c
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-28 20:28:03 +0200

    7diena

--- Diena7/Diena7/src/main/java/org/example/Main.java
+++ Diena7/Diena7/src/main/java/org/example/Main.java
@@ -3,9 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        double [] a = {2, 3, 66, 78};
+        Task.task2();
 
-        System.out.println("Lielākā vērtība ir: " + Task.task1(a));
+//        Sample.run();
+//
+//        double [] a = {2, 3, 66, 78};
+//
+//        System.out.println("Lielākā vērtība ir: " + Task.task1_1(a));
 
     }
 }
--- Diena7/Diena7/src/main/java/org/example/Task.java
+++ Diena7/Diena7/src/main/java/org/example/Task.java
@@ -1,6 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task {
+
+    public static void task2(){
+        Scanner sc = new Scanner(System.in);
+        String a = "";
+
+        System.out.println("Cik vērtības vēlieties pievienot listam?");
+        int count = sc.nextInt();
+        List<String> randomList = new ArrayList<>(count);
+
+
+        for(int i = 0; i < randomList.size(); i++){
+            System.out.println("Ievadi vēlamās string vērtības");
+            a = sc.nextLine();
+            randomList.add(a);
+        }
+
+    }
+
+    public static double task1_1(double [] arr){
+        double largest = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
 
     public static double task1(double [] arr){
         double answ = 0;
