5731e5fef5129b88c34643e089362f095ce4e7f5
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-28 19:23:25 +0200

    Update Task.java

--- Diena7/Diena7/src/main/java/org/example/Task.java
+++ Diena7/Diena7/src/main/java/org/example/Task.java
@@ -4,14 +4,12 @@ public class Task {
 
     public static double task1(double [] arr){
         double answ = 0;
-        
+
 
         for(int i = 0; i < arr.length; i++){
             if(arr[i] > answ){
                 answ-=answ;
                 answ+= arr[i];
-            }else if(arr[i] > answ){
-
             }
         }
         return answ;
