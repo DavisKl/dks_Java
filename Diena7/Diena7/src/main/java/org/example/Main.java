a3131613aaf889c6d42b25be372acff65da191ba
DavisKl <150376554+DavisKl@users.noreply.github.com>
2023-11-28 19:20:00 +0200

    7 diena

---
+++ Diena7/Diena7/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double [] a = {2, 3, 66, 78};
+
+        System.out.println("Lielākā vērtība ir: " + Task.task1(a));
+
+    }
+}
---
+++ Diena7/Diena7/src/main/java/org/example/Task.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Task {
+
+    public static double task1(double [] arr){
+        double answ = 0;
+        double chan = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > answ){
+                answ-=answ;
+                answ+= arr[i];
+            }else if(arr[i] > answ){
+
+            }
+        }
+        return answ;
+    }
+
+
+
+
+
+}
+
